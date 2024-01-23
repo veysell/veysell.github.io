ccfbb1ff8ecce4205709b1fc8c3addf0379f1b2c
erkanagir <erkan@humanits.net>
2024-01-24 02:39:27 +0300

    hastalık tespit

--- js/decisionTree.js
+++ js/decisionTree.js
@@ -1,39 +1,216 @@
 const decisionTree = {
-    "Göz Hastalıkları": {
-      "Göz kapağında çıkan kırmızı yumru var mı?": {
-        "Evet": "Arpacık",
-        "Hayır": {
-          "Gözlerde sulanma var mı?": {
-            "Evet": {
-              "Göz çevresinde ağrı ve şişlikler var mı?": {
-                "Evet": "Göz sulanması",
-                "Hayır": "Göz Kayması"
+  "Göz Hastalıkları": {
+    "Göz kapağında çıkan kırmızı yumru var mı?": {
+      "Evet": "Arpacık",
+      "Hayır": {
+        "Gözlerde sulanma var mı?": {
+          "Evet": {
+            "Göz çevresinde ağrı ve şişlikler var mı ?": {
+              "Evet": "Göz sulanması",
+              "Hayır": "Doktora başvurmalısınız"
+            }
+          },
+          "Hayır": {
+            "Gözlerde kayma var mı ?": {
+              "Evet": {
+                "Göz kırpma sıklığı var mı ?": {
+                  "Evet": "Göz tembelliği",
+                  "Hayır": "Doktora başvurmalısınız"
+                }
+              },
+              "Hayır": "Doktora başvurmalısınız"
+            }
+          }
+        }
+      }
+    }
+  },
+  "Boşaltım Sistemi Hastalıkları": {
+    "Ateş var mı?": {
+      "Evet": {
+        "Sık idrara çıkma ihtiyacı var mı?": {
+          "Evet": {
+            "Sulu kaka var mı?": {
+              "Evet": "İshal",
+              "Hayır": "Doktora başvurmalısınız"
+            }
+          },
+          "Hayır": {
+            "İdrar yaparken ağrı yada yanma var mı?": {
+              "Evet": "İdrar Yolu Enfeksiyonu",
+              "Hayır": "Prostat"
+            }
+          }
+        }
+      },
+      "Hayır": "Kabızlık"
+    },
+    "Hayır": {
+      "İdrara çıkamama durumu var mı ?": {
+        "Evet": "Kabızlık",
+        "Hayır": "Doktora başvurmalısınız"
+      }
+    }
+  },
+  "Kulak Burun Boğaz Hastalıkları": {
+    "Öksürük var mı ?": {
+      "Evet": {
+        "Yüksek ateş var mı ?": {
+          "Evet": {
+            "İştahsızlık var mı ?": {
+              "Evet": {
+                "Burun akıntısı": {
+                  "Evet": {
+                    "Göğüste hırıltı var mı ?": {
+                      "Evet": {
+                        "Nefes darlığı var mı ?": {
+                          "Evet": {
+                            "Dudaklar ve tırnakların morarması var mı ?": {
+                              "Evet": "RSV",
+                              "Hayır": "Doktora başvurunuz"
+                            }
+                          },
+                          "Hayır": {
+                            "Ağız çevresi ve parmak uçlarında morarma var mı?": {
+                              "Evet": "Bronşiyolit",
+                              "Hayır": "Doktora başvurunuz"
+                            }
+                          }
+                        }
+                      },
+                      "Hayır": "Doktora başvurunuz"
+                    }
+                  },
+                  "Hayır": "Doktora başvurunuz"
+                }
+              },
+              "Hayır": {
+                "Kas ağrıları, geçmeyen ateş var mı ?": {
+                  "Evet": "Influenza",
+                  "Hayır": "Doktora başvurunuz"
+                }
               }
-            },
-            "Hayır": "Göz Tembelliği"
+            }
+          },
+          "Hayır": "Doktora başvurunuz"
+        }
+      },
+      "Hayır": {
+        "Deride kaşıntı var mı ?": {
+          "Evet": {
+            "Kabartılı kızarıklık var mı ?": {
+              "Evet": {
+                "Eklemlerde ağrı var mı ?": {
+                  "Evet": "Ürtiker",
+                  "Hayır": "Doktora başvurunuz"
+                }
+              },
+              "Hayır": {
+                "İçinde sıvı ve irin olan yara var mı ?": {
+                  "Evet": "Impetigo",
+                  "Hayır": "Doktora başvurunuz"
+                }
+              }
+            }
+          },
+          "Hayır": {
+            "Kaşıntı, akıntı, ve hapşırık var mı ?": {
+              "Evet": "Alerji",
+              "Hayır": "Doktora başvurunuz"
+            }
           }
         }
       }
     },
-    "Boşaltım Sistemi Hastalıkları": {
-      "Ateş var mı?": {
+    "Hayır": {
+      "Nefes darlığı var mı ?": {
         "Evet": {
-          "Sık idrara çıkma ihtiyacı var mı?": {
+          "Göğüste hırıltı var mı ?": {
             "Evet": {
-              "Sulu kaka var mı?": {
-                "Evet": "İshal",
-                "Hayır": "Bakteriyel Enfeksiyon"
+              "Şiddetli solunum güçlüğü var mı ?": {
+                "Evet": "Astım",
+                "Hayır": "Doktora başvurunuz"
               }
             },
             "Hayır": {
-              "İdrar yaparken ağrı yada yanma var mı?": {
-                "Evet": "İdrar Yolu Enfeksiyonu",
-                "Hayır": "Prostat"
+              "Salya akıtma, yutmada zorluk var  mı ?": {
+                "Evet": "Yalancı kuş palazı",
+                "Hayır": "Doktora başvurunuz"
               }
             }
           }
         },
-        "Hayır": "Kabızlık"
+        "Hayır": {
+          "Burun akıntısı var mı?": {
+            "Evet": {
+              "Boğmaca": "Boğmaca",
+              "Hayır": "Doktora başvurunuz"
+            }
+          }
+        }
       }
     }
-  };
+  },
+  "Döküntüler ve Alerjiler": {
+    "Yüksek ateş var mı ?": {
+      "Evet": {
+        "Deride döküntü var mı ?": {
+          "Evet": {
+            "Kabartılı kızarıklık var mı ?": {
+              "Evet": {
+                "Bağışıklık sisteminde zayıflık var mı ?": {
+                  "Evet": "Beşinci hastalık",
+                  "Hayır": "Doktora başvurunuz"
+                }
+              },
+              "Hayır": {
+                "Ağız yaraları sıvı alımına engel oluyor mu?": {
+                  "Evet": "El ayak hastalığı",
+                  "Hayır": "Doktora başvurunuz"
+                }
+              }
+            }
+          },
+          "Hayır": {
+            "Kaşıntı, akıntı, ve hapşırık var mı ?": {
+              "Evet": {
+                "Kabartılı kızarıklık var mı ?": {
+                  "Evet": "Ürtiker",
+                  "Hayır": "Doktora başvurunuz"
+                }
+              },
+              "Hayır": {
+                "İçinde sıvı ve irin olan yara var mı ?": {
+                  "Evet": "Impetigo",
+                  "Hayır": "Doktora başvurunuz"
+                }
+              }
+            }
+          }
+        }
+      },
+      "Hayır": {
+        "Geçmeyen ateş ve halsizlik var mı ?": {
+          "Evet": "Enfeksiyon soğuk algınlığı",
+          "Hayır": "Doktora başvurunuz"
+        }
+      }
+    },
+    "Hayır": {
+      "Kulak ağrısı var mı ?": {
+        "Evet": "Kulak iltihabı",
+        "Hayır": {
+          "Ciltte ve gözlerde sararma var mı ?": {
+            "Evet": "Sarılık",
+            "Hayır": {
+              "Dilde beyaz lekeler var mı ?": {
+                "Evet": "Pamukçuk",
+                "Hayır": "Doktora başvurunuz"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+};
--- js/painDetected.js
+++ js/painDetected.js
@@ -1,88 +1,138 @@
+var symptomIds = {};
+var currentQuestionNode = null;
+
 document.getElementById('humanBody').addEventListener('click', function (event) {
-    var x = (event.clientX - this.getBoundingClientRect().left) / this.clientWidth * 100;
-    var y = (event.clientY - this.getBoundingClientRect().top) / this.clientHeight * 100;
-  
-    var selectedDisease = "";
-    
-    if (x >= 40 && x <= 50 && y >= 20 && y <= 30) {
-      selectedDisease = "Göz Hastalıkları";
-    } else if (x >= 55 && x <= 65 && y >= 10 && y <= 20) {
-      selectedDisease = "Boşaltım Sistemi Hastalıkları";
-    } else {
-      hideResult();
-      return;
-    }
-  
-    askQuestions(decisionTree[selectedDisease]);
-  });
-  
-  function askQuestions(node) {
-    var question = Object.keys(node)[0];
-    var answers = node[question];
-  
-    var userAnswer = prompt(question + " (Evet/Hayır)").toLowerCase();
-    console.log(userAnswer);
-    if (answers[userAnswer]) {
-      if (typeof answers[userAnswer] === 'string') {
-        displayResult(answers[userAnswer], 'Description of ' + answers[userAnswer] + '...', []);
-      } else {
-        askQuestions(answers[userAnswer]);
-      }
-    } else {
-      hideResult();
-    }
+  var x = (event.clientX - this.getBoundingClientRect().left) / this.clientWidth * 100;
+  var y = (event.clientY - this.getBoundingClientRect().top) / this.clientHeight * 100;
+  console.log(x + "--" + y);
+  var selectedDisease = "";
+
+  if ((x >= 40 && x <= 45 && y >= 20 && y <= 25) || (x >= 55 && x <= 62 && y >= 20 && y <= 25)) {
+    hideResult();
+    selectedDisease = "Göz Hastalıkları";
+  } else if (x >= 40 && x <= 60 && y >= 55 && y <= 65) {
+    hideResult();
+    selectedDisease = "Boşaltım Sistemi Hastalıkları";
   }
-  
-  function displayResult(title, description, symptoms) {
-    // Display the result on the right side
-    var resultContent = document.getElementById('resultContent');
-    resultContent.innerHTML = `
-        <h4>${title}</h4>
-        <p>${description}</p>
-    `;
+  else if ((x >= 30 && x <= 35 && y >= 22 && y <= 28) || (x >= 67 && x <= 73 && y >= 22 && y <= 28)
+    || (x >= 47 && x <= 49 && y >= 26 && y <= 29) || (x >= 39 && x <= 61 && y >= 37 && y <= 47)) {
+    hideResult();
+    selectedDisease = "Kulak Burun Boğaz Hastalıkları";
+  }
+  else {
+    hideResult();
+    return;
+  }
 
-    if (symptoms && symptoms.length > 0) {
-        // Display symptoms dynamically in the form
-        var symptomForm = document.getElementById('symptomForm');
-        symptomForm.innerHTML = ''; // Clear previous content
+  currentQuestionNode = decisionTree[selectedDisease];
+  askCurrentQuestion();
+});
 
-        symptoms.forEach(function (symptom, index) {
-            // Generate unique ID for each symptom
-            var symptomId = index.toString();
-            symptomIds[symptom] = symptomId;
+function askCurrentQuestion() {
+  var question = Object.keys(currentQuestionNode)[0];
+  var questionText = document.getElementById('questionText');
+  questionText.innerText = question;
 
-            var checkbox = document.createElement('div');
-            checkbox.className = 'form-check';
-            checkbox.innerHTML = `
-                <input class="form-check-input" type="checkbox" value="${symptom}" id="${symptomId}">
-                <label class="form-check-label" for="${symptomId}">${symptom}</label>
-            `;
-            symptomForm.appendChild(checkbox);
-        });
+  // Show the question container
+  document.getElementById('questionContainer').style.display = 'block';
+}
+
+function answerQuestion(userAnswer) {
+  // Hide the question container
+  document.getElementById('questionContainer').style.display = 'none';
+
+  var answers = currentQuestionNode[Object.keys(currentQuestionNode)[0]];
+
+  if (answers[userAnswer]) {
+    if (typeof answers[userAnswer] === 'string') {
+      Swal.fire({
+        title: answers[userAnswer],
+        text: 'Lütfen en doğru sonuç için Hekiminize başvurunuz... Geçmiş Olsun',
+        icon: 'success',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: "Kapat",
+        confirmButtonText: 'MHRS Sistemine Gitmek İstiyorum'
+      }).then(function (result) {
+        if (result.isConfirmed) {
+          location.assign("https://mhrs.gov.tr/vatandas/")
+        }
+      });
+      //displayResult(answers[userAnswer], 'Description of ' + answers[userAnswer] + '...', []);
+    } else {
+      currentQuestionNode = answers[userAnswer];
+      askCurrentQuestion();
     }
+  } else {
+    hideResult();
+  }
+}
 
-    // resultContent'i görünür yap
-    document.getElementById('resultContainer').style.display = 'block';
-    document.getElementById('symptoms').style.display = 'block';
+function displayResult(title, description, symptoms) {
+  // Display the result on the right side
+  var resultContent = document.getElementById('resultContent');
+  resultContent.innerHTML = `
+                <h4>${title}</h4>
+                <p>${description}</p>
+            `;
 
-    // Hide all painInfo elements
-    hideAllPainInfo();
+  if (symptoms && symptoms.length > 0) {
+    // Display symptoms dynamically in the form
+    var symptomForm = document.getElementById('symptomForm');
+    symptomForm.innerHTML = ''; // Clear previous content
+
+    symptoms.forEach(function (symptom, index) {
+      // Generate unique ID for each symptom
+      var symptomId = index.toString();
+      symptomIds[symptom] = symptomId;
+
+      var checkbox = document.createElement('div');
+      checkbox.className = 'form-check';
+      checkbox.innerHTML = `
+                        <input class="form-check-input" type="checkbox" value="${symptom}" id="${symptomId}">
+                        <label class="form-check-label" for="${symptomId}">${symptom}</label>
+                    `;
+      symptomForm.appendChild(checkbox);
+    });
+  }
+
+  // resultContent'i görünür yap
+  document.getElementById('resultContainer').style.display = 'block';
+  document.getElementById('symptoms').style.display = 'block';
+
+  // Hide all painInfo elements
+  hideAllPainInfo();
 }
 
 function hideResult() {
-    // Hide the result on the right side
-    document.getElementById('resultContainer').style.display = 'none';
-
-    // Hide all painInfo elements
-    hideAllPainInfo();
+  // Hide the result on the right side
+  document.getElementById('resultContainer').style.display = 'none';
+  document.getElementById('questionContainer').style.display = 'none';
+  // Hide all painInfo elements
+  hideAllPainInfo();
 }
 
 function hideAllPainInfo() {
-    // Hide all painInfo elements
-    var painInfos = document.getElementsByClassName('painInfo');
-    for (var i = 0; i < painInfos.length; i++) {
-        painInfos[i].style.display = 'none';
-    }
+  // Hide all painInfo elements
+  var painInfos = document.getElementsByClassName('painInfo');
+  for (var i = 0; i < painInfos.length; i++) {
+    painInfos[i].style.display = 'none';
+  }
 }
 
-  
+function getSelectedSymptoms() {
+  var symptoms = [];
+  var checkboxes = document.querySelectorAll('#symptomForm input[type="checkbox"]:checked');
+  checkboxes.forEach(function (checkbox) {
+    symptoms.push({
+      id: symptomIds[checkbox.value],
+      name: checkbox.value
+    });
+  });
+
+  // Display selected symptoms in the result container
+  var symptomsText = symptoms.length > 0 ? symptoms.map(symptom => `${symptom.name} (ID: ${symptom.id})`).join(', ') : 'Belirtiler seçilmedi.';
+  displayResult('Seçilen Belirtiler', symptomsText, symptoms);
+  document.getElementById('symptoms').style.display = 'none';
+}
